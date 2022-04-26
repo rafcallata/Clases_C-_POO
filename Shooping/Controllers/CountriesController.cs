21bebbb79bed2c3ade70e355a29a8baf1c005d0a
rafcallata <rafcallataf@upt.pe>
2022-04-26 13:48:51 -0500

    Categories CRUD Finished

--- Shooping/Controllers/CountriesController.cs
+++ Shooping/Controllers/CountriesController.cs
@@ -1,177 +1,172 @@
 ﻿#nullable disable
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Shooping.Data;
 using Shooping.Data.Entities;
 
 namespace Shooping.Controllers
 {
-    public class CountriesController : Controller
-    {
-        private readonly DataContext _context;
+	public class CountriesController : Controller
+	{
+		private readonly DataContext _context;
 
-        public CountriesController(DataContext context)
-        {
-            _context = context;
-        }
+		public CountriesController(DataContext context)
+		{
+			_context = context;
+		}
 
-        // GET: Countries
-        [HttpGet]
-        public async Task<IActionResult> Index()
-        {
-            return View(await _context.Countries.ToListAsync());
-        }
+		// GET: Countries
+		[HttpGet]
+		public async Task<IActionResult> Index()
+		{
+			return View(await _context.Countries.ToListAsync());
+		}
 
-        // GET: Countries/Details/5
-        [HttpGet]
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
+		// GET: Countries/Details/5
+		[HttpGet]
+		public async Task<IActionResult> Details(int? id)
+		{
+			if (id == null)
+			{
+				return NotFound();
+			}
 
-            Country country = await _context.Countries
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (country == null)
-            {
-                return NotFound();
-            }
+			Country country = await _context.Countries
+				.FirstOrDefaultAsync(m => m.Id == id);
+			if (country == null)
+			{
+				return NotFound();
+			}
 
-            return View(country);
-        }
+			return View(country);
+		}
 
-        // GET: Countries/Create
-        [HttpGet]
-        public IActionResult Create()
-        {
-            return View();
-        }
+		// GET: Countries/Create
+		[HttpGet]
+		public IActionResult Create()
+		{
+			return View();
+		}
 
-        // POST: Countries/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create(Country country)
-        {
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Add(country);
-                    await _context.SaveChangesAsync();
-                    return RedirectToAction(nameof(Index));
-                }
-                catch (DbUpdateException dbUpdateException)
-                {
-                    if (dbUpdateException.InnerException.Message.Contains("duplicate"))
-                    {
-                        ModelState.AddModelError(string.Empty, "Ya existe un país con el mismo nombre.");
-                    }
-                    else
-                    {
-                        ModelState.AddModelError(string.Empty, dbUpdateException.InnerException.Message);
-                    }
-                }
-                catch (Exception exception)
-                {
-                    ModelState.AddModelError(string.Empty, exception.Message);
-                }
-            }
-            return View(country);
-        }
+		// POST: Countries/Create
+		// To protect from overposting attacks, enable the specific properties you want to bind to.
+		// For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> Create(Country country)
+		{
+			if (ModelState.IsValid)
+			{
+				try
+				{
+					_context.Add(country);
+					await _context.SaveChangesAsync();
+					return RedirectToAction(nameof(Index));
+				}
+				catch (DbUpdateException dbUpdateException)
+				{
+					if (dbUpdateException.InnerException.Message.Contains("duplicate"))
+					{
+						ModelState.AddModelError(string.Empty, "Ya existe un país con el mismo nombre.");
+					}
+					else
+					{
+						ModelState.AddModelError(string.Empty, dbUpdateException.InnerException.Message);
+					}
+				}
+				catch (Exception exception)
+				{
+					ModelState.AddModelError(string.Empty, exception.Message);
+				}
+			}
+			return View(country);
+		}
 
-        // GET: Countries/Edit/5
-        [HttpGet]
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
+		// GET: Countries/Edit/5
+		[HttpGet]
+		public async Task<IActionResult> Edit(int? id)
+		{
+			if (id == null)
+			{
+				return NotFound();
+			}
 
-            Country country = await _context.Countries.FindAsync(id);
-            if (country == null)
-            {
-                return NotFound();
-            }
-            return View(country);
-        }
+			Country country = await _context.Countries.FindAsync(id);
+			if (country == null)
+			{
+				return NotFound();
+			}
+			return View(country);
+		}
 
-        // POST: Countries/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, Country country)
-        {
-            if (id != country.Id)
-            {
-                return NotFound();
-            }
+		// POST: Countries/Edit/5
+		// To protect from overposting attacks, enable the specific properties you want to bind to.
+		// For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> Edit(int id, Country country)
+		{
+			if (id != country.Id)
+			{
+				return NotFound();
+			}
 
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(country);
-                    await _context.SaveChangesAsync();
-                    return RedirectToAction(nameof(Index));
-                }
-                catch (DbUpdateException dbUpdateException)
-                {
-                    if (dbUpdateException.InnerException.Message.Contains("duplicate"))
-                    {
-                        ModelState.AddModelError(string.Empty, "Ya existe un país con el mismo nombre.");
-                    }
-                    else
-                    {
-                        ModelState.AddModelError(string.Empty, dbUpdateException.InnerException.Message);
-                    }
-                }
-                catch (Exception exception)
-                {
-                    ModelState.AddModelError(string.Empty, exception.Message);
-                }
+			if (ModelState.IsValid)
+			{
+				try
+				{
+					_context.Update(country);
+					await _context.SaveChangesAsync();
+					return RedirectToAction(nameof(Index));
+				}
+				catch (DbUpdateException dbUpdateException)
+				{
+					if (dbUpdateException.InnerException.Message.Contains("duplicate"))
+					{
+						ModelState.AddModelError(string.Empty, "Ya existe un país con el mismo nombre.");
+					}
+					else
+					{
+						ModelState.AddModelError(string.Empty, dbUpdateException.InnerException.Message);
+					}
+				}
+				catch (Exception exception)
+				{
+					ModelState.AddModelError(string.Empty, exception.Message);
+				}
 
-            }
-            return View(country);
-        }
+			}
+			return View(country);
+		}
 
-        // GET: Countries/Delete/5
-        [HttpGet]
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
+		// GET: Countries/Delete/5
+		[HttpGet]
+		public async Task<IActionResult> Delete(int? id)
+		{
+			if (id == null)
+			{
+				return NotFound();
+			}
 
-            Country country = await _context.Countries
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (country == null)
-            {
-                return NotFound();
-            }
+			Country country = await _context.Countries
+				.FirstOrDefaultAsync(m => m.Id == id);
+			if (country == null)
+			{
+				return NotFound();
+			}
 
-            return View(country);
-        }
+			return View(country);
+		}
 
-        // POST: Countries/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            var country = await _context.Countries.FindAsync(id);
-            _context.Countries.Remove(country);
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }        
-    }
+		// POST: Countries/Delete/5
+		[HttpPost, ActionName("Delete")]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> DeleteConfirmed(int id)
+		{
+			var country = await _context.Countries.FindAsync(id);
+			_context.Countries.Remove(country);
+			await _context.SaveChangesAsync();
+			return RedirectToAction(nameof(Index));
+		}
+	}
 }
--- Shooping/Data/DataContext.cs
+++ Shooping/Data/DataContext.cs
@@ -9,10 +9,12 @@ namespace Shooping.Data
         {
         }
         public DbSet<Country> Countries { get; set; }
+        public DbSet<Category> Categories { get; set; }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
             modelBuilder.Entity<Country>().HasIndex(c => c.Name).IsUnique();
+            modelBuilder.Entity<Category>().HasIndex(c => c.Name).IsUnique();
         }
     }
 }
