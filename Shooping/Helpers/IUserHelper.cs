c0d97838481c151fbb5fbfc99b313a01c59b7501
rafcallata <rafcallataf@upt.pe>
2022-04-30 23:02:21 -0500

    Confirm email and reset password

--- Shooping/Helpers/IUserHelper.cs
+++ Shooping/Helpers/IUserHelper.cs
@@ -10,9 +10,7 @@ namespace Shooping.Helpers
 
         Task<IdentityResult> AddUserAsync(User user, string password);
         Task<User> AddUserAsync(AddUserViewModel model);
-
         Task CheckRoleAsync(string roleName);
-
         Task AddUserToRoleAsync(User user, string roleName);
 
         Task<bool> IsUserInRoleAsync(User user, string roleName);
@@ -22,6 +20,12 @@ namespace Shooping.Helpers
         Task<IdentityResult> ChangePasswordAsync(User user, string oldPassword, string newPassword);
         Task<IdentityResult> UpdateUserAsync(User user);
         Task<User> GetUserAsync(Guid userId);
+
+        Task<string> GenerateEmailConfirmationTokenAsync(User user);
+        Task<IdentityResult> ConfirmEmailAsync(User user, string token);
+
+        Task<string> GeneratePasswordResetTokenAsync(User user);
+        Task<IdentityResult> ResetPasswordAsync(User user, string token, string password);
 
     }
 }
