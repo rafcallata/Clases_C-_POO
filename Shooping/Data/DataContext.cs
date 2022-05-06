476ea663edb7e5bb1544154b0527eadfb7fa973d
rafcallata <rafcallataf@upt.pe>
2022-05-06 11:40:59 -0500

    Add products to card

--- Shooping/Data/DataContext.cs
+++ Shooping/Data/DataContext.cs
@@ -16,6 +16,7 @@ namespace Shooping.Data
         public DbSet<Product> Products { get; set; }
         public DbSet<ProductCategory> ProductCategories { get; set; }
         public DbSet<ProductImage> ProductImages { get; set; }
+        public DbSet<TemporalSale> TemporalSales { get; set; }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
